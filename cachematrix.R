d0742ddd3793a580150f2c1cf49617511fb7ab2f
James Kranig <jmkranig@yahoo.com>
2015-03-16 20:06:39 -0400

    Defined makeCacheMAtrix and cacheSolve functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,72 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions makeCacheMatrix() and cacheSolve()
+## work together to determine the inverse of an
+## invertible matrix and cache the inverted matrix
+## for better performance. 
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## mackeCacheMatrix() creates a wrapper around
+## a standard R matrix, binding the functions 
+## set(), get() (for setting and getting the 
+## underlying R matrix) and setMatInv() and 
+## getMatInv() for setting and getting the 
+## calculated inverse of the wrapped matrix.
+## Parameters:
+## mat - an R matrix to be wrapped.
+makeCacheMatrix <- function(mat = matrix()) {
 
+    matInv <- NULL
+    set <- function(matToSet) {
+        
+        mat <<- matToSet
+        matInv <<- NULL
+    }
+    
+    
+    get <- function() { 
+        
+        mat 
+    }
+    
+    
+    setMatInv <- function(matInvToSet) { 
+        
+        matInv <<- matInvToSet 
+    }
+    
+    
+    getMatInv <- function() {
+        
+        matInv
+    }
+    
+    list(set = set, 
+         get = get, 
+         setMatInv = setMatInv, 
+         getMatInv = getMatInv)
+    
 }
+ 
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve() determines the inverse of the matrix 
+## cachingMat if it was NOT been determined previously
+## and caches the resulting inverted matrix; otherwise, 
+## cacheSolve() will simply return the cached inverted matrix.
+## Parameters:
+## cachingMat - a "cachedMatrix" produced by the 
+##              makeCacheMatrix function.
+cacheSolve <- function(cachingMat, ...) {
+    
+    matInv <- cachingMat$getMatInv()
+    if (!is.null(matInv)) {
+        
+        message("Getting cached matrix inverse.")
+        return (matInv)
+    }
+    
+    mat <- cachingMat$get()
+    matInv <- solve(mat)
+    cachingMat$setMatInv(matInv)
+    
+    matInv
+        
 }
